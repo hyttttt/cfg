33b1004fff06c6797797952a766b529351b7c3ad
b10930032@ms.ntust.edu.tw <b10930032@ms.ntust.edu.tw>
2023-03-22 17:15:05 +0800

    feat: response in json

--- services/responseService.go
+++ services/responseService.go
@@ -1,1 +1,11 @@
 package services
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func ResponseWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Write(response)
+}
