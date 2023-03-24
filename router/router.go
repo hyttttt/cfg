da229ec125254e0438f81248cea533b56f88477b
MirrorShih <jess2529156651@gmail.com>
2023-03-24 14:50:36 +0800

    refactor: add GET callback for init

--- router/router.go
+++ router/router.go
@@ -5,14 +5,36 @@ import (
 	"net/http"
 )
 
-func NewRouter() *gin.Engine {
-	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
-	router.Static("/static", "./static")
-	router.GET("/", func(c *gin.Context) {
+type Route struct {
+	Method  func(engine *gin.Engine, path string, handler func(c *gin.Context))
+	Path    string
+	Handler func(c *gin.Context)
+}
+
+var routes []Route
+
+func GET(engine *gin.Engine, path string, handler func(c *gin.Context)) {
+	engine.GET(path, handler)
+}
+
+func register(method func(engine *gin.Engine, path string, handler func(c *gin.Context)), path string, handler func(c *gin.Context)) {
+	routes = append(routes, Route{method, path, handler})
+}
+
+func init() {
+	register(GET, "/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"title": "CFG Viewer",
 		})
 	})
+}
+
+func NewRouter() *gin.Engine {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.Static("/static", "./static")
+	for _, route := range routes {
+		route.Method(router, route.Path, route.Handler)
+	}
 	return router
 }
