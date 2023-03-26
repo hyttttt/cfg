5fd8e7699479566456ee608676df9038dc3d9fe3
hyttttt <bianca20011228@gmail.com>
2023-03-26 18:18:00 +0800

    feat: revise index page and add cfg page

---
+++ static/js/index.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+  // load the binaries we have and make it a list
+  loadList();
+};
+
+// load the binaries we have and make it a list
+function loadList() {
+  var myList = document.getElementById("myList");
+
+  for (i = 0; i < 30; i++) {
+    const newLi = document.createElement("li");
+    newLi.innerHTML = "New List Item " + i;
+    newLi.className = "list-group-item";
+
+    newLi.onmouseover = function () {
+      this.className = "list-group-item active";
+    };
+
+    newLi.onmouseout = function () {
+      this.className = "list-group-item";
+    };
+
+    myList.appendChild(newLi);
+  }
+}
