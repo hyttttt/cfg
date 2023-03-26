ce3e375cb13a1a59ee59e2a62bf82fdb88ec1a74
MirrorShih <jess2529156651@gmail.com>
2023-03-26 21:34:21 +0800

    feat: add cfg page and refactor the structure

--- controller/controller.go
+++ controller/controller.go
@@ -7,17 +7,7 @@ import (
 	"os/exec"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-type Binary struct {
-	Id       primitive.ObjectID
-	Hash     string
-	FuncName string
-	Dot      string
-}
 
 func HomePage(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{
@@ -25,49 +15,20 @@ func HomePage(c *gin.Context) {
 	})
 }
 
-func ReturnFunction(c *gin.Context) {
-	client, ctx, cancel, err := services.ConnectMongo("")
-	if err != nil {
-		panic(err)
-	}
-	defer services.CloseMongo(client, ctx, cancel) //release resources
-	services.PingMongo(client, ctx)
-
-	collection := client.Database("cfg").Collection("")
-
-	target := c.Param("hash")
-	result := Binary{}
-	filter := bson.D{{"hash", target}}
-	err = collection.FindOne(ctx, filter).Decode(&result)
-
-	if err != nil {
-		panic(err)
-	}
-	c.JSON(http.StatusOK, gin.H{"status": "success", "cfg_id:": &result.Id, "function name": &result.FuncName})
+func CFGPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "cfg.html", gin.H{
+		"title": c.Param("hash"),
+	})
 }
 
-func ReturnHashList(c *gin.Context) {
-	client, ctx, cancel, err := services.ConnectMongo("")
-	if err != nil {
-		panic(err)
-	}
-	defer services.CloseMongo(client, ctx, cancel) //release resources
-	services.PingMongo(client, ctx)
-
-	collection := client.Database("cfg").Collection("")
-
-	filter := bson.D{}
-	opts := options.Find().SetProjection(bson.D{{"hash", 1}})
-	cursor, err := collection.Find(ctx, filter, opts)
-	if err != nil {
-		panic(err)
-	}
-	var result []Binary
-	if err = cursor.All(ctx, &result); err != nil {
-		panic(err)
+func Hash(c *gin.Context) {
+	target := c.Query("hash")
+	if target == "" {
+		services.ReturnHashList(c)
+	} else {
+		services.ReturnFunction(c, target)
 	}
 
-	c.JSON(http.StatusOK, gin.H{"status": "success", "list:": &result})
 }
 
 func AnalyzeBinary(c *gin.Context) {
--- services/responseService.go
+++ services/responseService.go
@@ -2,7 +2,62 @@ package services
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"net/http"
 )
+
+type Binary struct {
+	Id       primitive.ObjectID
+	Hash     string
+	FuncName string
+	Dot      string
+}
+
+func ReturnFunction(c *gin.Context, target string) {
+	client, ctx, cancel, err := ConnectMongo("")
+	if err != nil {
+		panic(err)
+	}
+	defer CloseMongo(client, ctx, cancel) //release resources
+	PingMongo(client, ctx)
+
+	collection := client.Database("cfg").Collection("")
+
+	result := Binary{}
+	filter := bson.D{{"hash", target}}
+	err = collection.FindOne(ctx, filter).Decode(&result)
+
+	if err != nil {
+		panic(err)
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "success", "cfg_id:": &result.Id, "function name": &result.FuncName})
+}
+
+func ReturnHashList(c *gin.Context) {
+	client, ctx, cancel, err := ConnectMongo("")
+	if err != nil {
+		panic(err)
+	}
+	defer CloseMongo(client, ctx, cancel) //release resources
+	PingMongo(client, ctx)
+
+	collection := client.Database("cfg").Collection("")
+
+	filter := bson.D{}
+	opts := options.Find().SetProjection(bson.D{{"hash", 1}})
+	cursor, err := collection.Find(ctx, filter, opts)
+	if err != nil {
+		panic(err)
+	}
+	var result []Binary
+	if err = cursor.All(ctx, &result); err != nil {
+		panic(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "success", "list:": &result})
+}
 
 func ResponseInJson(c *gin.Context) {
 
