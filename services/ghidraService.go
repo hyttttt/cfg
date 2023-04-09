d844cdd51ba4abc39be0a2dcc8af8a79105f0d48
MirrorShih <jess2529156651@gmail.com>
2023-04-09 20:17:37 +0800

    feat: avoid ghidra open same project

--- services/ghidraService.go
+++ services/ghidraService.go
@@ -3,10 +3,14 @@ package services
 import (
 	"log"
 	"os/exec"
+	"strconv"
 )
 
+var project_number = 1
+
 func AnalyzeHeadless(scriptPath string, binaryPath string) {
-	cmd := exec.Command("../ghidra_10.2.3_PUBLIC/support/analyzeHeadless", "/home/gradle/", "project", "-scriptPath", "./ghidra_scripts/", "-postscript", scriptPath, "-import", binaryPath, "-deleteProject")
+	cmd := exec.Command("../ghidra_10.2.3_PUBLIC/support/analyzeHeadless", "/home/gradle/", "project"+strconv.Itoa(project_number), "-scriptPath", "./ghidra_scripts/", "-postscript", scriptPath, "-import", binaryPath, "-deleteProject")
+	project_number++
 	err := cmd.Run()
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
