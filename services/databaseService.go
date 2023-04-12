f29c7719b36a09f99820a42d0adea8aa1a31ffb1
MirrorShih <jess2529156651@gmail.com>
2023-04-12 17:55:41 +0800

    refactor: create Mongo Client for remove redundant connection

--- services/databaseService.go
+++ services/databaseService.go
@@ -1,13 +1,65 @@
 package services
 
 import (
+	"cfg/models"
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
+	"os"
 	"time"
 )
+
+type MongoDBClient struct {
+	client *mongo.Client
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+var mongoClient = NewMongoDBClient(os.Getenv("MONGODB_URI"))
+
+func NewMongoDBClient(uri string) *MongoDBClient {
+	client, ctx, cancel, err := ConnectMongo(uri)
+	if err != nil {
+		log.Println("connect mongodb failed")
+		log.Println(err.Error())
+	}
+	if PingMongo(client, ctx) != nil {
+		log.Println("ping failed")
+		log.Println(err.Error())
+	}
+	return &MongoDBClient{client, ctx, cancel}
+}
+
+func (mongoClient *MongoDBClient) FindOne(databaseName string, collectionName string, filter bson.D) *models.Binary {
+	collection := mongoClient.client.Database(databaseName).Collection(collectionName)
+	var result *models.Binary
+	err := collection.FindOne(mongoClient.ctx, filter).Decode(&result)
+	if err != nil {
+		log.Println("find data failed")
+		log.Println(err.Error())
+	}
+	return result
+}
+
+func (mongoClient *MongoDBClient) List(databaseName string, collectionName string) []models.Binary {
+	collection := mongoClient.client.Database(databaseName).Collection(collectionName)
+	cursor, err := collection.Find(mongoClient.ctx, bson.D{})
+	if err != nil {
+		panic(err)
+	}
+	var results []models.Binary
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+	if err != nil {
+		log.Println("find data failed")
+		log.Println(err.Error())
+	}
+	return results
+}
 
 func ConnectMongo(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
 	ctx, cancel := context.WithTimeout(context.Background(),
--- services/responseService.go
+++ services/responseService.go
@@ -1,73 +1,24 @@
 package services
 
 import (
-	"cfg/models"
-	"net/http"
-	"os"
-
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"net/http"
 )
 
 func GetFunction(c *gin.Context, target string) {
-	uri := os.Getenv("MONGODB_URI")
-	client, ctx, cancel, err := ConnectMongo(uri)
-	if err != nil {
-		panic(err)
-	}
-	defer CloseMongo(client, ctx, cancel) //release resources
-	PingMongo(client, ctx)
-
-	collection := client.Database("cfg").Collection("binary")
-
-	var result *models.Binary
 	filter := bson.D{bson.E{Key: "hash", Value: target}}
-	err = collection.FindOne(ctx, filter).Decode(&result)
-
-	if err != nil {
-		panic(err)
-	}
+	result := mongoClient.FindOne("cfg", "binary", filter)
 	c.JSON(http.StatusOK, gin.H{"status": "success", "cfg_id:": result.Id, "function name": result.FuncName})
 }
 
 func GetHashList(c *gin.Context) {
-	uri := os.Getenv("MONGODB_URI")
-	client, ctx, cancel, err := ConnectMongo(uri)
-	if err != nil {
-		panic(err)
-	}
-	defer CloseMongo(client, ctx, cancel) //release resources
-	PingMongo(client, ctx)
-
-	collection := client.Database("cfg").Collection("binary")
-
-	var result *models.Binary
-	filter := bson.D{bson.E{Key: "hash", Value: true}}
-	err = collection.FindOne(ctx, filter).Decode(&result)
-
-	if err != nil {
-		panic(err)
-	}
-	c.JSON(http.StatusOK, gin.H{"list": result})
+	results := mongoClient.List("cfg", "binary")
+	c.JSON(http.StatusOK, gin.H{"list": results})
 }
 
 func GetDot(c *gin.Context, target string) {
-	uri := os.Getenv("MONGODB_URI")
-	client, ctx, cancel, err := ConnectMongo(uri)
-	if err != nil {
-		panic(err)
-	}
-	defer CloseMongo(client, ctx, cancel) //release resources
-	PingMongo(client, ctx)
-
-	collection := client.Database("cfg").Collection("binary")
-
-	var result *models.Binary
 	filter := bson.D{bson.E{Key: "cfg_id", Value: target}}
-	err = collection.FindOne(ctx, filter).Decode(&result)
-
-	if err != nil {
-		panic(err)
-	}
+	result := mongoClient.FindOne("cfg", "binary", filter)
 	c.JSON(http.StatusOK, gin.H{"cfg_id": target, "dot": result.Dot})
 }
