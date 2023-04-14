9e242e91bf834a4671a86f77ced456018915848c
MirrorShih <jess2529156651@gmail.com>
2023-04-14 14:28:22 +0800

    feat: complete correct function return

--- services/responseService.go
+++ services/responseService.go
@@ -3,22 +3,29 @@ package services
 import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
 )
 
 func GetFunction(c *gin.Context, target string) {
 	filter := bson.D{bson.E{Key: "hash", Value: target}}
-	result := mongoClient.FindBinary("cfg", "binary", filter)
-	c.JSON(http.StatusOK, gin.H{"status": "success", "cfg_id:": result.Id, "function name": result.FuncName})
+	projection := bson.D{{"cfg_id", 1}, {"function", 1}}
+	results := mongoClient.ListBinaries("cfg", "binary", filter, projection)
+	var response []bson.M
+	for _, res := range results {
+		response = append(response, bson.M{"cfg_id": res.Id.Hex(), "function": res.FuncName})
+	}
+	c.JSON(http.StatusOK, response)
 }
 
 func GetHashList(c *gin.Context) {
-	results := mongoClient.ListBinaries("cfg", "binary")
-	c.JSON(http.StatusOK, gin.H{"list": results})
+	results := mongoClient.ListField("cfg", "binary", "hash")
+	c.JSON(http.StatusOK, results)
 }
 
 func GetDot(c *gin.Context, target string) {
-	filter := bson.D{bson.E{Key: "cfg_id", Value: target}}
+	id, _ := primitive.ObjectIDFromHex(target)
+	filter := bson.D{bson.E{Key: "_id", Value: id}}
 	result := mongoClient.FindBinary("cfg", "binary", filter)
-	c.JSON(http.StatusOK, gin.H{"cfg_id": target, "dot": result.Dot})
+	c.JSON(http.StatusOK, gin.H{"cfg_id": result.Id.Hex(), "dot": result.Dot})
 }
